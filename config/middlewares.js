38af335eddd4717dc2de78791d54cd7f6efaada5
scalemailadam <dreidelmadebeats@gmail.com>
2026-03-17 19:52:14 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,34 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:3000',
+        'https://scalemail.vercel.app',
+        'https://scalemailstudio.com',
+        'https://www.scalemailstudio.com',
+        'https://scalemail-cms.onrender.com',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
