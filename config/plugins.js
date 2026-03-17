bb1f00ecccb21d4a983421f8bd270a6869f52fcd
scalemailadam <dreidelmadebeats@gmail.com>
2026-03-17 20:59:44 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,18 @@
 module.exports = ({ env }) => ({
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
   upload: {
     config: {
       provider: '@strapi/provider-upload-cloudinary',
